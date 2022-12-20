d2ba6d6bb378b82796b2128775f53e6ad69341d0
Gregory Santana <gregorymhk@gmail.com>
2022-12-20 14:37:19 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+</head>
+<body>
+
+<h2>This is a heading</h2>
+
+<table style="width:100%">
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+    <th>Action</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+    <td><button class="delete-button" data-id="123">Delete</button></td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+    <td><button class="delete-button" data-id="124">Delete</button></td>
+  </tr>
+</table>
+
+</body>
+</html>
